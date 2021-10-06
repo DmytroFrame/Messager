208d1dad036536bf9b6e16cfd789f5027d0a5b20
Dmytro Kostyshyn <dima.kostishen@gmail.com>
2021-10-06 17:13:55 +0300

    create decorator GetUsername

---
+++ src/decorators/username.decorator.ts
@@ -0,0 +1,9 @@
+import { createParamDecorator, ExecutionContext } from "@nestjs/common";
+
+
+export const GetUsername = createParamDecorator(
+    (data: unknown, ctx: ExecutionContext) => {
+        const request = ctx.switchToHttp().getRequest()
+        return request.user
+    }
+)
